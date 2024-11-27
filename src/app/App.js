ed6a2c669a171a20b525efb1f41e746c1538fb14
DanielDeanGithub <github-new@inboxrefresh.com>
2024-11-27 07:54:46 +0000

    Add Categories route

--- src/app/App.js
+++ src/app/App.js
@@ -20,6 +20,7 @@ const router = createBrowserRouter(
       <Route path='/about' element={ <About/> } />
       <Route path='/sign-up' element={ <SignUp/> } />
       <Route path='/articles' element={ <Articles/> } />
+      <Route path='/categories' element={ <Categories/> } />
     </Route>
   )
 );
