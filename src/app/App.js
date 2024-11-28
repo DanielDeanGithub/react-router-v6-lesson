7e0db114e74b96770886c324d20f7b1e86a84e61
DanielDeanGithub <github-new@inboxrefresh.com>
2024-11-28 08:25:33 +0000

    Add Author dynamic route

--- src/app/App.js
+++ src/app/App.js
@@ -17,12 +17,13 @@ import "./App.css";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={ <Root/> }>
-      <Route path='/about' element={ <About/> } />
-      <Route path='/sign-up' element={ <SignUp/> } />
-      <Route path='/articles' element={ <Articles/> } />
-      <Route path='/articles/:title' element={ <Article/> } />
-      <Route path='/categories' element={ <Categories/> } />
-      <Route path='/profile' element={ <Profile/> } />
+      <Route path='about' element={ <About/> } />
+      <Route path='sign-up' element={ <SignUp/> } />
+      <Route path='articles' element={ <Articles/> } />
+      <Route path='articles/:title' element={ <Article/> } />
+      <Route path='authors/:name' element={ <Author/> } />
+      <Route path='categories' element={ <Categories/> } />
+      <Route path='profile' element={ <Profile/> } />
     </Route>
   )
 );
