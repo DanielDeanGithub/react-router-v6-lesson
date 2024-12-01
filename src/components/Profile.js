0eca7c0fed2463b68c27d2b21069799d1be32a88
DanielDeanGithub <github-new@inboxrefresh.com>
2024-12-01 18:33:37 +0000

    Navigate users not logged in to the sign-up page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,7 +7,11 @@ export default function Profile () {
   const currentUser = useSelector(selectCurrentUser)
   const loggedIn = useSelector(selectIsLoggedIn);
   
-  // use loggedIn to return a Navigate
+  if (!loggedIn) {
+    return (
+      <Navigate to='/sign-up' />
+    );
+  };
 
   return (
     <main>
