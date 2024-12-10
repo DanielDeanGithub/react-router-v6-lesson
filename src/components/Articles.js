6fc7c2867595977e4d6c2d4febd0dd4a22ecc922
DanielDeanGithub <github-new@inboxrefresh.com>
2024-12-10 12:05:21 +0000

    Update title variable to be result of searchParams.get('title')

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -12,7 +12,7 @@ export default function Articles () {
   const [ searchParams ] = useSearchParams();
   
   // Get the queryParams from object returned from useSearchParams and set to `title`
-  const title = '';
+  const title = searchParams.get('title');
 
   const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
 
